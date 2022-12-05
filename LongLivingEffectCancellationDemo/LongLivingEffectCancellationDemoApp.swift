8a128855ecc6a22cb4d3c9d73835cf660c0680e8
Ryan Zulkoski <ryan.zulkoski@buildertrend.com>
2022-12-05 09:51:30 -0600

    Initial Commit

---
+++ LongLivingEffectCancellationDemo/LongLivingEffectCancellationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongLivingEffectCancellationDemoApp.swift
+//  LongLivingEffectCancellationDemo
+//
+//  Created by Ryan Zulkoski on 12/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LongLivingEffectCancellationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
